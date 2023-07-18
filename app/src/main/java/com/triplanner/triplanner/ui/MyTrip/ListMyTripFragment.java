1ffcae4fcc18e22d9c7e143bc7086f6f41eefc7c
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-07-18 21:19:10 +0300

    הצגת מפה של גוגל

--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListMyTripFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListMyTripFragment.java
@@ -1,66 +1,127 @@
 package com.triplanner.triplanner.ui.MyTrip;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+
+import com.triplanner.triplanner.LoginActivity;
+import com.triplanner.triplanner.MainActivity;
+import com.triplanner.triplanner.Model.Model;
+import com.triplanner.triplanner.Model.Trip;
 import com.triplanner.triplanner.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListMyTripFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.User;
+
 public class ListMyTripFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListMyTripFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListMyTripFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListMyTripFragment newInstance(String param1, String param2) {
-        ListMyTripFragment fragment = new ListMyTripFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    Trip[] arrayTrip;
+    TextView name,destination,numDays,date;
+    ListView listViewTrip;
+//    MyAdapter adapter;
+    User user;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_my_trip, container, false);
+        View view=inflater.inflate(R.layout.fragment_list_my_trip, container, false);
+        Realm.init(getContext()); // context, usually an Activity or Application
+        App app = new App(new AppConfiguration.Builder(getString(R.string.AppId)).build());
+        user = app.currentUser();
+
+        Button btn_map= view.findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+//        Model.instance.getAllTrip(user.getProfile().getEmail(), getContext(), new Model.GetAllTripListener() {
+//            @Override
+//            public void onComplete(Trip[] trips) {
+//                arrayTrip=trips;
+//                listViewTrip = view.findViewById(R.id.fragment_list_my_trip_list);
+//                adapter=new MyAdapter();
+//                listViewTrip.setAdapter(adapter);
+//                listViewTrip.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+//                        Model.instance.getAllPlacesOfTrip(arrayTrip[i].getId_trip(), getContext(), new Model.GetAllPlacesOfTrip() {
+//                            @Override
+//                            public void onComplete(Place[] places) {
+//                                ListMyTripFragmentDirections.ActionNavMyTripToListDayInTripFragment action=ListMyTripFragmentDirections.actionNavMyTripToListDayInTripFragment(arrayTrip[i].getTripName(),arrayTrip[i].getTripDestination(),arrayTrip[i].getTripDaysNumber(),places);
+//                                Navigation.findNavController(view).navigate( action);
+//                            }
+//                        });
+//
+//                    }
+//                });
+//            }
+//        });
+
+
+        return view;
     }
+//    class MyAdapter extends BaseAdapter {
+//
+//        public MyAdapter(){
+//
+//        }
+//        @Override
+//        public int getCount() {
+//            if (arrayTrip== null) {
+//                return 0;
+//            } else {
+//                return arrayTrip.length;
+//            }
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return null;
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return 0;
+//        }
+//
+//        @Override
+//        public View getView(int i, View view, ViewGroup viewGroup) {
+//            if (view == null) {
+//                LayoutInflater inflater = getLayoutInflater();
+//                view = inflater.inflate(R.layout.my_plan_row, null);
+//            } else {
+//
+//            }
+//            Trip trip =arrayTrip[i];
+//
+//            name = view.findViewById(R.id.my_plan_row_name);
+//            name.setText(trip.getTripName());
+//            destination= view.findViewById(R.id.my_plan_row_destination);
+//            destination.setText(trip.getTripDestination());
+//            numDays = view.findViewById(R.id.my_plan_row_num_days);
+//            numDays.setText(String.valueOf(trip.getTripDaysNumber()));
+//            date=view.findViewById(R.id.my_plan_row_date);
+//            date.setText(trip.getDate());
+//
+//            return view;
+//        }
+//
+//    }
 }

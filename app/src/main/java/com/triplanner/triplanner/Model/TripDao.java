778a33a6594c6c1f7ed50564431ecd9e02a6cd04
tehila cohen <tehilamc@gmail.com>
2023-08-01 14:40:14 +0300

    כפתור ניווט לוויז

--- app/src/main/java/com/triplanner/triplanner/Model/TripDao.java
+++ app/src/main/java/com/triplanner/triplanner/Model/TripDao.java
@@ -1,10 +1,12 @@
 package com.triplanner.triplanner.Model;
 
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import com.triplanner.triplanner.Model.Trip;
 import java.util.List;
 
 @Dao
--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListMyTripFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListMyTripFragment.java
@@ -89,3 +89,4 @@ public class ListMyTripFragment extends Fragment {
 
     }
 }
+

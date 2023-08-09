e99864ff29c260a32de822cacee17121a979f8d5
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-08-09 17:54:54 +0300

    טיול ביום עיצוב

--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListTripInDayFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListTripInDayFragment.java
@@ -17,11 +17,22 @@ import com.triplanner.triplanner.R;
 
 public class ListTripInDayFragment extends Fragment {
 
+    TextView name,numDay;
+    ImageView imagev;
+
+    Place[] arrayPlaces;
+    ListView listViewPlaces;
+    String destination;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list_trip_in_day, container, false);
+        listViewPlaces =view.findViewById(R.id.fragment_list_trip_in_day_list_view);
+        arrayPlaces =  ListTripInDayFragmentArgs.fromBundle(getArguments()).getArrPlaces();
+        destination= ListTripInDayFragmentArgs.fromBundle(getArguments()).getTripDestination();
+        numDay = view.findViewById(R.id.fragment_list_trip_in_day_num_day);
+        numDay.setText("Day "+String.valueOf(arrayPlaces[0].getDay_in_trip()));
         return view;
     }
 

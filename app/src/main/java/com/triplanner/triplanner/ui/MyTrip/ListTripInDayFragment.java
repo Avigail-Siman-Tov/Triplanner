55d033d59bea21fc2066371117b1ff2ec2325f19
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-08-09 19:59:21 +0300

    יצירת רשימה של טיולים ביום

--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListTripInDayFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListTripInDayFragment.java
@@ -2,11 +2,15 @@ package com.triplanner.triplanner.ui.MyTrip;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
@@ -19,10 +23,11 @@ public class ListTripInDayFragment extends Fragment {
 
     TextView name,numDay;
     ImageView imagev;
-
     Place[] arrayPlaces;
+
     ListView listViewPlaces;
     String destination;
+    MyAdapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,7 +38,42 @@ public class ListTripInDayFragment extends Fragment {
         destination= ListTripInDayFragmentArgs.fromBundle(getArguments()).getTripDestination();
         numDay = view.findViewById(R.id.fragment_list_trip_in_day_num_day);
         numDay.setText("Day "+String.valueOf(arrayPlaces[0].getDay_in_trip()));
+        adapter=new MyAdapter();
+        listViewPlaces.setAdapter(adapter);
+        listViewPlaces.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+
+            }
+        });
         return view;
+    }
+
+
+    class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            if (arrayPlaces == null) {
+                return 0;
+            } else {
+                return arrayPlaces.length;
+            }
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 
 }

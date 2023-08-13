3a6cee295d75354e1c8560e7a1ae3a364121b96c
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-08-13 10:37:26 +0300

    הוספת תכונה במחלקת תכנון מקום וניתובים

--- app/src/main/java/com/triplanner/triplanner/Model/PlacePlanning.java
+++ app/src/main/java/com/triplanner/triplanner/Model/PlacePlanning.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class PlacePlanning extends PlaceDetails implements Parcelable {
     private boolean status;
+    private int day_in_trip;
 
     public PlacePlanning() {
     }
@@ -13,10 +14,14 @@ public class PlacePlanning extends PlaceDetails implements Parcelable {
     public PlacePlanning(String placeID, String placeName, double placeLocationLat, double placeLocationLng, String placeFormattedAddress, String placeInternationalPhoneNumber, List<String> placeOpeningHours, float placeRating, String placeWebsite, String placeImgUrl, boolean status) {
         super(placeID, placeName, placeLocationLat, placeLocationLng, placeFormattedAddress, placeInternationalPhoneNumber,  placeOpeningHours, placeRating,  placeWebsite,  placeImgUrl);
         this.status=status;
+        this.day_in_trip = 0;
+
     }
 
     protected PlacePlanning(Parcel in) {
+
         status = in.readByte() != 0;
+        day_in_trip = in.readInt();
     }
 
 
@@ -42,6 +47,20 @@ public class PlacePlanning extends PlaceDetails implements Parcelable {
     @Override
     public int describeContents() {
         return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeByte((byte) (status ? 1 : 0));
+        dest.writeInt(day_in_trip);
+    }
+    public int getDay_in_trip() {
+        return day_in_trip;
+    }
+
+    public void setDay_in_trip(int day_in_trip) {
+        this.day_in_trip = day_in_trip;
     }
 
 

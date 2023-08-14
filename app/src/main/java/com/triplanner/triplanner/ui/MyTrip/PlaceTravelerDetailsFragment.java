3f0737d499449917818d63af3e7ab1d74d8d4f04
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-08-14 23:22:13 +0300

    חלק מהפרטים על הטיול

--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/PlaceTravelerDetailsFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/PlaceTravelerDetailsFragment.java
@@ -1,10 +1,14 @@
 package com.triplanner.triplanner.ui.MyTrip;
 
 import android.app.ProgressDialog;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +16,14 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.triplanner.triplanner.Model.Model;
 import com.triplanner.triplanner.Model.Place;
 import com.triplanner.triplanner.R;
 
 import org.json.JSONObject;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlaceTravelerDetailsFragment extends Fragment {
     Place place;
@@ -44,6 +52,22 @@ public class PlaceTravelerDetailsFragment extends Fragment {
         placeName.setText(place.getPlaceName());
         placeAddress.setText(place.getPlaceFormattedAddress());
         myLoadingDialog=new ProgressDialog(getContext());
+        Model.instance.getOpenHoursOfPlace(place.getPlaceID(), getContext(), new Model.GetOpenHoursOfPlaceListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onComplete(List<String> result) {
+
+                String openingHours = result.stream().map(n -> String.valueOf(n))
+                        .collect(Collectors.joining("\n", "", ""));
+                placeOpeningHours.setText(openingHours);
+            }
+        });
+        String weblink = place.getPlaceWebsite();
+        if(weblink!=null && weblink!="") {
+            placeWebsite.setMovementMethod(LinkMovementMethod.getInstance());
+            String text = "<a href='"+weblink+"'> Link </a>";
+            placeWebsite.setText(Html.fromHtml(text));
+        }
         return view;
     }
 

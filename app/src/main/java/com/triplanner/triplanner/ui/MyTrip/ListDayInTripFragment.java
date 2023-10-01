0f276dcf3445ee1d9a1a12920319b41f1c09c4f3
tehila cohen <tehilamc@gmail.com>
2023-10-01 14:47:49 +0300

    שער signup

--- app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListDayInTripFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/MyTrip/ListDayInTripFragment.java
@@ -1,10 +1,10 @@
 package com.triplanner.triplanner.ui.MyTrip;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

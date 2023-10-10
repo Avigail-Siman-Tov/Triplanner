f70568a06a7d0d86fc9d09327263233b1d460279
tehila cohen <tehilamc@gmail.com>
2023-10-10 22:36:07 +0300

    העלאת תמונה אישית

--- app/src/main/java/com/triplanner/triplanner/ui/profile/TravelerProfileFragment.java
+++ app/src/main/java/com/triplanner/triplanner/ui/profile/TravelerProfileFragment.java
@@ -1,32 +1,31 @@
 package com.triplanner.triplanner.ui.profile;
-
-
+import android.content.Intent;
+import android.net.Uri;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Build;
-import android.os.Bundle;
-
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
+import java.io.IOException;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import com.triplanner.triplanner.Model.Model;
 import com.triplanner.triplanner.Model.Traveler;
 import com.triplanner.triplanner.R;
-
-
 import java.util.List;
-
 import io.realm.Realm;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
@@ -40,6 +39,10 @@ public class TravelerProfileFragment extends Fragment {
     MyAdapter adapter;
     String [] arrCategory;
     TextView category,logout;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView profileImage; // Add this line
+
     final String[] categoriesArray={
             "amusement park","aquarium","art gallery","bar","casino",
             "museum","night club","park","shopping mall","spa",
@@ -56,11 +59,13 @@ public class TravelerProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_traveler_profile, container, false);
-//        categories = view.findViewById(R.id.traveler_profile_categories);
+       //categories = view.findViewById(R.id.traveler_profile_categories);
         editBtn = view.findViewById(R.id.traveler_profile_edit_btn);
         mail = view.findViewById(R.id.traveler_profile_email);
         name = view.findViewById(R.id.traveler_profile_name);
         listCategory=view.findViewById(R.id.traveler_profile_list_category);
+        profileImage = view.findViewById(R.id.profileImage); // Add this line
+
         Realm.init(getContext()); // context, usually an Activity or Application
         App app = new App(new AppConfiguration.Builder(getString(R.string.AppId)).build());
         User user = app.currentUser();
@@ -69,6 +74,20 @@ public class TravelerProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(view).navigate(TravelerProfileFragmentDirections.actionNavProfileToLogoutActivity());
+            }
+        });
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isNetworkConnected()) {
+                    // ... (your existing code)
+
+                    // Add the following lines to launch the image selection
+                    Intent imagePickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(imagePickerIntent, PICK_IMAGE_REQUEST);
+                } else {
+                    Toast.makeText(getContext(), "Error! Connect to Internet", Toast.LENGTH_SHORT).show();
+                }
             }
         });
        Model.instance.getTravelerByEmailInDB(user.getProfile().getEmail(), getContext(), new Model.GetTravelerByEmailListener() {
@@ -99,6 +118,23 @@ public class TravelerProfileFragment extends Fragment {
         });
 
         return view;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                // User picked an image from the gallery
+                Uri selectedImage = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
+                    profileImage.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
